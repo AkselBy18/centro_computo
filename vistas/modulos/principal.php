287d74b6cf7a37b53d1d2a3a950e71c7b0658048
angelcastro <angelcastroarellano15@gmail.com>
2022-07-14 20:10:29 -0600

    agregamos el apartado principal a las ramas

---
+++ vistas/modulos/principal.php
@@ -0,0 +1,10 @@
+<div class="jumbotron">
+  <h1 class="display-4">centro de computo</h1>
+  <p class="lead">Encuentra el software que estas buscando aqui con nosotros.</p>
+  <hr class="my-4">
+  <p>en este sitio podras ver o alquilar software en grandes cantidades</p>
+</div>
+<br><br>
+        <img src="https://static8.depositphotos.com/1007429/885/i/950/depositphotos_8855148-stock-photo-row-of-hard-drives.jpg" width="530" height="350">
+        <img src="https://st2.depositphotos.com/1145386/6840/i/950/depositphotos_68408547-stock-photo-hard-drives-in-data-center.jpg" width="530" height="350">
+</div>
