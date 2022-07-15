f6ab7c2ee2b18ce216836f840f54e647bc4f283e
Akemi Herrera <106628476+AkselBy18@users.noreply.github.com>
2022-07-14 19:16:00 -0600

    Agregue el menu

---
+++ vistas/modulos/menu.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">centro</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?opcion=principal">Principal</a>
+        </li>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?opcion=Centro_Computo">Centro de Computo</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Altas
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="index.php?opcion=alta_usuarios">Usuario</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_software">software</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_control">control</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=actualizar_usuario">actualizar usuario</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=entregar_software">entregar software</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Mostrar
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="index.php?opcion=listado_software">listado de software</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=listado_prestamos">listado de software prestado</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=listado_sancionados">listado de sancionados</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=listado_control">listado de control</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
